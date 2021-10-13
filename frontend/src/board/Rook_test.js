c8adfecc1c21ba207265a38e968a1ef4a856753f
PiraMIID <55114984+PiraMIID@users.noreply.github.com>
2021-10-14 00:42:04 +0200

    fix tests

--- frontend/src/board/Rook_test.js
+++ frontend/src/board/Rook_test.js
@@ -1,11 +1,14 @@
 
 import React from 'react'
 
-
 import '@testing-library/jest-dom'
+
 
 let matrix = Array.from((8,8));
 let rookActual = new Rook('white', 2, 2);
+
+
+
 
 describe('checking method of valid path', () => {
     expect(rookActual.isMoveCorrect(matrix,3,7)).toEqual(false)
